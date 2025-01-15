e8d025f701dd5a1ccf9022350bc4e428f2286d10
Deiv237 <didiuldeivid@gmail.com>
2025-01-15 22:48:22 +0200

    Workout

--- JavaScript/Workout-Journal/routers/apiRouter.js
+++ JavaScript/Workout-Journal/routers/apiRouter.js
@@ -1,3 +1,15 @@
 const express = require("express");
 
 const apiRouter = express.Router();
+
+apiRouter.route(`/`);
+apiRouter.route("/users/:id").get();
+apiRouter.route("/users/:username").get();
+apiRouter.route("/users").get();
+apiRouter.route("users/:id/workouts").get();
+apiRouter.route("users/workouts").get();
+apiRouter.route("users/workouts/:id").get();
+apiRouter.route("users/workouts/:id/exercise").get();
+apiRouter.route("users/workouts/:workout_id/exercise/:id").put();
+apiRouter.route("/workouts/exercises/:id").delete();
+apiRouter.route("users/profile").get();

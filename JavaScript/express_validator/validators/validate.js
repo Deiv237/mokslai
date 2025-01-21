d031c1fb30662200c39e91237c8fef128766e612
Deiv237 <didiuldeivid@gmail.com>
2025-01-21 22:51:55 +0200

    Add 3-rd Validate option

--- JavaScript/express_validator/validators/validate.js
+++ JavaScript/express_validator/validators/validate.js
@@ -35,3 +35,23 @@ module.exports = validate;
 // };
 
 // module.exports = validate;
+
+// const { validationResult } = require('express-validator');
+// const AppError = require('../utils/appError');
+
+// const validate = (req, res, next) => {
+//     const errors = validationResult(req);
+    
+//     if (!errors.isEmpty()) {
+//         const errorMessages = errors
+//             .array()
+//             .map(error => error.msg)
+//             .join('; ');
+
+//         return next(new AppError(errorMessages, 400));
+//     }
+
+//     next();
+// };
+
+// module.exports = validate;
